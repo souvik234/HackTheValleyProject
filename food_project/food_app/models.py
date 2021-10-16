f23816837b21f7e4268728721ddf549639361d9d
souvik234 <souvikphy.mukherjee@mail.utoronto.ca>
2021-10-16 16:48:18 -0400

    Fixed Expiry Date

--- food_project/food_app/models.py
+++ food_project/food_app/models.py
@@ -1,4 +1,16 @@
+import datetime
+
 from django.db import models
+
+expiryDict={
+    'Apples': 7,
+    'Milk': 30,
+    'Bananas': 7,
+    'Eggs': 14,
+    'lettuce': 12
+
+
+}
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
@@ -9,7 +21,18 @@ class ItemPurchased(models.Model):
     quantity = models.FloatField()
     photo = models.ImageField(upload_to='items')
 
-    expiry_date = date_purchased.date()
+    expiry_date = models.DateField(blank=True, null=True)
+
+
+    def clean(self):
+        if not self.expiry_date:
+            self.expiry_date = self.date_purchased + datetime.timedelta(days=expiryDict['Apples'])
+
+    def save(self, **kwargs):
+        self.clean()
+        return super().save(**kwargs)
+
+
     def __str__(self):
         return self.name
 
